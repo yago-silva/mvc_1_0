e422150e9927e7c14df97e741f9a1a4e35ad0920
Yago Silva <login+yago-silva@elo7.com>
2015-07-20 13:11:19 -0300

    Adiciona classe para configuração da aplicação mvc

---
+++ src/br/com/devmedia/mvc/config/MinhaApplicacaoMvc.java
@@ -0,0 +1,8 @@
+package br.com.devmedia.mvc.config;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("mvc")
+class MinhaApplicacaoMvc extends Application{
+}
