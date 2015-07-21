d1fb829fbf8a92def581d19e9b5297757737ac5e
Yago Silva <login+yago-silva@elo7.com>
2015-07-20 22:19:43 -0300

    Adiciona método para retornar view utilizando Viewable no controlador ola mundo

--- src/br/com/devmedia/mvc/olamundo/OlaMundoController.java
+++ src/br/com/devmedia/mvc/olamundo/OlaMundoController.java
@@ -2,6 +2,7 @@ package br.com.devmedia.mvc.olamundo;
 
 import javax.mvc.Controller;
 import javax.mvc.View;
+import javax.mvc.Viewable;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
@@ -19,5 +20,11 @@ public class OlaMundoController{
 	@Path("exibe_mesma_pagina")
 	@View("ola_mundo.jsp")
 	public void exibeMesmaPagina(){
+	}
+	
+	@GET
+	@Path("exibe_pagina_com_viewable")
+	public Viewable exibePaginaComViewable(){
+		return new Viewable("ola_mundo.jsp");    
 	}
 }
